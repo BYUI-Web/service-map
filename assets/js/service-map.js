be96bcbb667971a37cde79ddefdcf88a39308f14
Matthew Sessions <shichongrui@Matthews-MacBook-Pro.local>
2014-09-24 12:04:19 -0600

    Finished the service map

--- assets/js/service-map.js
+++ assets/js/service-map.js
@@ -11,11 +11,14 @@ serviceMap.controller('ServicesSearch', ['$scope', "$filter", "dataService",
         dataService.getData(function (data) {
             $scope.services = data.services; // Add data to scope
             $scope.allTags = data.tags;
+            $scope.departments = data.departments;
+
             $scope.numPages = function () {
                 var filteredServices = $scope.services;
                 if ($scope.query !== "") {
                     filteredServices = $filter("filter")(filteredServices, $scope.query);
                 }
+                filteredServices = $filter("filterTags")(filteredServices, $scope.allTags, $scope.departments);
                 return Math.ceil(filteredServices.length / $scope.servicesPerPage);
             };
         });
@@ -55,24 +58,63 @@ serviceMap.filter("isSelected", function () {
     }
 });
 
-serviceMap.filter("filterTags", function () {
-    return function (input, tags) {
-        var selectedTags = {};
-        var response = [];
+serviceMap.filter("filterTags", ["$filter", function ($filter) {
+    return function (input, tags, departments) {
+        var selectedTags = [],
+            selectedDepartments = [],
+            response = input,
+            found = false,
+            numSelectedTags;
+
+        if (input === undefined) {
+            return input;
+        }
+
+
         if (tags !== undefined) {
             Object.keys(tags).forEach(function (tag) {
                 if (tags[tag] !== false) {
-                    selectedTags[tag] = true;
+                    selectedTags.push(tag);
                 }
             });
         }
-        if (input !== undefined) {
-            input.forEach(function(service) {
-                
+
+        if (departments !== undefined) {
+            Object.keys(departments).forEach(function (department) {
+                if (departments[department] !== false) {
+                    selectedDepartments.push(department);
+                }
             });
         }
+
+        if (selectedTags.length > 0) {
+            response = $filter("filter")(response, function(value) {
+                var found = false;
+                selectedTags.forEach(function(tag) {
+                    if (value.tags.indexOf(tag) !== -1) {
+                        found = true;
+                        return true;
+                    }
+                });
+                return found;
+            });
+        }
+        
+        if (selectedDepartments.length > 0) {
+            response = $filter("filter")(response, function(value) {
+                var found = false;
+                selectedDepartments.forEach(function(department) {
+                    if (value.sponsor === department) {
+                        found = true;
+                        return true;
+                    }
+                });
+                return found;
+            });
+        }
+        return response;
     }
-});
+}]);
 
 serviceMap.factory("dataService", ["$http",
         function ($http) {
@@ -90,9 +132,22 @@ serviceMap.factory("dataService", ["$http",
             return tags;
         }
 
+        function extractAllDepartments(services) {
+            var departments = {};
+
+            services.forEach(function (service) {
+                if (departments[service.sponsor] === undefined) {
+                    departments[service.sponsor] = false;
+                }
+            });
+
+            return departments;
+        }
+
         service.getData = function (cb) {
             $http.get('data/services.json').success(function (data) {
                 data.tags = extractAllTags(data.services);
+                data.departments = extractAllDepartments(data.services);
                 cb(data);
             }).error(cb);
         };
