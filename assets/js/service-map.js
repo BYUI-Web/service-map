2b14a4c4b8aa5e06b00aa18605bab60d11174423
Matthew Sessions <shichongrui@Matthews-MacBook-Pro.local>
2014-09-24 09:05:33 -0600

    Got tagging to show up, now implementing filter to filter the services based on what tags are selected

--- assets/js/service-map.js
+++ assets/js/service-map.js
@@ -1,20 +1,22 @@
 var serviceMap = angular.module('serviceMap', []);
 
-serviceMap.controller('ServicesSearch', ['$scope', '$http', "$filter",
-    function ($scope, $http) {
+serviceMap.controller('ServicesSearch', ['$scope', "$filter", "dataService",
+
+    function ($scope, $filter, dataService) {
         $scope.services = [];
         $scope.servicesPerPage = 1;
         $scope.currentPage = 1;
         $scope.query = "";
-        
-        $http.get('data/services.json').success(function (data) {
+
+        dataService.getData(function (data) {
             $scope.services = data.services; // Add data to scope
-            $scope.allTags = data.allTags;
-            $scope.numPages = function() {
-                var filteredServices = $filter("filter")($scope.services, function($value) {
-                    return ;
-                })
-                return Math.ceil($scope.services.length / $scope.servicesPerPage);
+            $scope.allTags = data.tags;
+            $scope.numPages = function () {
+                var filteredServices = $scope.services;
+                if ($scope.query !== "") {
+                    filteredServices = $filter("filter")(filteredServices, $scope.query);
+                }
+                return Math.ceil(filteredServices.length / $scope.servicesPerPage);
             };
         });
 
@@ -27,6 +29,7 @@ serviceMap.controller('ServicesSearch', ['$scope', '$http', "$filter",
                 $scope.currentPage = n;
             }
         };
+
 }]);
 
 //We already have a limitTo filter built-in to angular,
@@ -37,3 +40,63 @@ serviceMap.filter('startFrom', function () {
         return input.slice(start);
     }
 });
+
+serviceMap.filter("isSelected", function () {
+    return function (input) {
+        var response = {};
+        if (input !== undefined) {
+            Object.keys(input).forEach(function (tag) {
+                if (input[tag] !== false) {
+                    response[tag] = true;
+                }
+            });
+        }
+        return response;
+    }
+});
+
+serviceMap.filter("filterTags", function () {
+    return function (input, tags) {
+        var selectedTags = {};
+        var response = [];
+        if (tags !== undefined) {
+            Object.keys(tags).forEach(function (tag) {
+                if (tags[tag] !== false) {
+                    selectedTags[tag] = true;
+                }
+            });
+        }
+        if (input !== undefined) {
+            input.forEach(function(service) {
+                
+            });
+        }
+    }
+});
+
+serviceMap.factory("dataService", ["$http",
+        function ($http) {
+        var service = {};
+
+        function extractAllTags(services) {
+            var tags = {};
+            services.forEach(function (service) {
+                service.tags.forEach(function (tag) {
+                    if (tags[tag] === undefined) {
+                        tags[tag] = false;
+                    }
+                });
+            });
+            return tags;
+        }
+
+        service.getData = function (cb) {
+            $http.get('data/services.json').success(function (data) {
+                data.tags = extractAllTags(data.services);
+                cb(data);
+            }).error(cb);
+        };
+
+        return service;
+
+}]);
