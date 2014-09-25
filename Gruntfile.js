055e2672cb0a09bdfb2027cc3e01a16dcbffbece
Matthew Sessions <shichongrui@gmail.com>
2014-09-25 08:43:19 -0600

    "autocommit"

---
+++ Gruntfile.js
@@ -0,0 +1,111 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        uglify: {
+            dev: {
+                options: {
+                    compress: false,
+                    beautify: true,
+                    mangle: false
+                },
+                files: {
+                    "assets/js/service-map.min.js": "assets/js/service-map.js"
+                }
+            },
+            prod: {
+                files: {
+                    "assets/js/service-map.min.js": "assets/js/service-map.js"
+                }
+            }
+        },
+
+        cssmin: {
+            prod: {
+                files: {
+                    "assets/css/services.min.css": "assets/css/services.css"
+                }
+            }
+        },
+
+        copy: {
+            dev: {
+                files: {
+                    "assets/css/services.min.css": "assets/css/services.css"
+                }
+            },
+            build: {
+                src: ["./**", "!./build/**", "!./node_modules/**"],
+                dest: "build/"
+            }
+        },
+        
+        clean: {
+            build: {
+                options: {
+                    force: true
+                },
+                src: "build"
+            }  
+        },
+
+        connect: {
+            server: {
+                options: {
+                    port: 5100,
+                    base: '.',
+                    livereload: true
+                }
+            },
+            keep: {
+                options: {
+                    port: 5100,
+                    base: ".",
+                    keepalive: true
+                }
+            }
+        },
+
+        git_deploy: {
+            prod: {
+                options: {
+                    url: "https://github.com/BYUI-Web/service-map.git"
+                },
+                src: "build"
+            }
+        },
+
+        watch: {
+            js: {
+                files: ['./assets/js/*.js', './assets/js/*.json', "!./assets/js/*.min.js"],
+                options: {
+                    livereload: true
+                },
+                tasks: ["uglify:dev"]
+            },
+            css: {
+                files: ["./assets/css/*.css", "!./assets/css/*.min.css"],
+                options: {
+                    livereload: true
+                },
+                tasks: ["copy:dev"]
+            },
+            html: {
+                files: ["**/*.html"],
+                options: {
+                    livereload: true
+                }
+            }
+        },
+    });
+
+    require("load-grunt-tasks")(grunt);
+
+    grunt.registerTask("default", ["uglify:prod", "cssmin:prod"]);
+    grunt.registerTask('dev', ['connect:server', "uglify:dev", "copy:dev", 'watch']);
+    grunt.registerTask("prod-test", ["uglify:prod", "cssmin:prod", "connect:keep"]);
+    grunt.registerTask("gh-pages", ["copy:build", "git_deploy", "clean:build"]);
+
+};
---
+++ README.md
@@ -0,0 +1,21 @@
+Service Map Prototype
+=====================
+
+This is a visual prototype for the BYU-Idaho Service Map. This map's purpose is to make it easier to find and access services across campus. Whether that service is available online or in person, each entry should make clear how the student can contact the department in charge of the service and begin to take advantage of what it has to offer.
+
+## Angular Prototype ##
+
+This high-fidelity prototype is built on Angular.js, and uses a static JSON file for data. The ideal implementation of this app will probably use a more dynamic data delivery method and a JavaScript library (or vanilla JS) better suited to the app.
+
+## Live Preview ##
+
+A live preview of the more recent versions of this prototype can be viewed at: <http://byui-web.github.io/service-map/>
+
+## Running Locally ##
+
+To locally view the framework, simply clone the repository, cd to the directory using the terminal, and run a simple server:
+```
+$ python -m SimpleHTTPServer
+```
+    
+
