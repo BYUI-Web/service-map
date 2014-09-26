7f5a16cf0fcacd9311ac072d5cdfe56590042807
Matthew Sessions <shichongrui@gmail.com>
2014-09-26 10:33:49 -0600

    Got the CSV and got it into arrays. Now just trying to get it to be in the right format

--- assets/js/service-map.js
+++ assets/js/service-map.js
@@ -23,6 +23,8 @@ serviceMap.controller('ServicesSearch', ['$scope', "$filter", "dataService",
             };
         });
 
+        dataService.getDataFromSheets();
+
         $scope.range = function (n) {
             return new Array(n);
         };
@@ -58,62 +60,63 @@ serviceMap.filter("isSelected", function () {
     }
 });
 
-serviceMap.filter("filterTags", ["$filter", function ($filter) {
-    return function (input, tags, departments) {
-        var selectedTags = [],
-            selectedDepartments = [],
-            response = input,
-            found = false,
-            numSelectedTags;
+serviceMap.filter("filterTags", ["$filter",
+    function ($filter) {
+        return function (input, tags, departments) {
+            var selectedTags = [],
+                selectedDepartments = [],
+                response = input,
+                found = false,
+                numSelectedTags;
 
-        if (input === undefined) {
-            return input;
-        }
+            if (input === undefined) {
+                return input;
+            }
 
 
-        if (tags !== undefined) {
-            Object.keys(tags).forEach(function (tag) {
-                if (tags[tag] !== false) {
-                    selectedTags.push(tag);
-                }
-            });
-        }
-
-        if (departments !== undefined) {
-            Object.keys(departments).forEach(function (department) {
-                if (departments[department] !== false) {
-                    selectedDepartments.push(department);
-                }
-            });
-        }
-
-        if (selectedTags.length > 0) {
-            response = $filter("filter")(response, function(value) {
-                var found = false;
-                selectedTags.forEach(function(tag) {
-                    if (value.tags.indexOf(tag) !== -1) {
-                        found = true;
-                        return true;
+            if (tags !== undefined) {
+                Object.keys(tags).forEach(function (tag) {
+                    if (tags[tag] !== false) {
+                        selectedTags.push(tag);
                     }
                 });
-                return found;
-            });
-        }
-        
-        if (selectedDepartments.length > 0) {
-            response = $filter("filter")(response, function(value) {
-                var found = false;
-                selectedDepartments.forEach(function(department) {
-                    if (value.sponsor === department) {
-                        found = true;
-                        return true;
+            }
+
+            if (departments !== undefined) {
+                Object.keys(departments).forEach(function (department) {
+                    if (departments[department] !== false) {
+                        selectedDepartments.push(department);
                     }
                 });
-                return found;
-            });
+            }
+
+            if (selectedTags.length > 0) {
+                response = $filter("filter")(response, function (value) {
+                    var found = false;
+                    selectedTags.forEach(function (tag) {
+                        if (value.tags.indexOf(tag) !== -1) {
+                            found = true;
+                            return true;
+                        }
+                    });
+                    return found;
+                });
+            }
+
+            if (selectedDepartments.length > 0) {
+                response = $filter("filter")(response, function (value) {
+                    var found = false;
+                    selectedDepartments.forEach(function (department) {
+                        if (value.sponsor === department) {
+                            found = true;
+                            return true;
+                        }
+                    });
+                    return found;
+                });
+            }
+            return response;
         }
-        return response;
-    }
 }]);
 
 serviceMap.factory("dataService", ["$http",
@@ -151,7 +154,113 @@ serviceMap.factory("dataService", ["$http",
                 cb(data);
             }).error(cb);
         };
+            
+//"serviceName": "sponsor": "shortName": "url": 
+//"contactName": "office": "contact": "mapUrl": 
+//"tags": "abstract":
+
+        service.getDataFromSheets = function (cb) {
+            $http.get("https://docs.google.com/spreadsheet/pub?key=0Aj1ydhtVRj-7dFo5eHdwQ1piMExaQ1p0b3JwbFJmLUE&single=true&gid=0&output=csv").success(function (data) {
+                var rows = CSVToArray(data).slice(3);
+                
+                rows = rows.map(function(row) {
+                    var newRow = {};
+                    newRow.serviceName = row[0];
+                    newRow.sponsor = row[3];
+                    newRow.shortName = row[1];
+                    newRow.url = row[2];
+                    newRow.url = row;
+                });
+            });
+        }
 
         return service;
 
 }]);
+
+// ref: http://stackoverflow.com/a/1293163/2343
+// This will parse a delimited string into an array of
+// arrays. The default delimiter is the comma, but this
+// can be overriden in the second argument.
+function CSVToArray(strData, strDelimiter) {
+    // Check to see if the delimiter is defined. If not,
+    // then default to comma.
+    strDelimiter = (strDelimiter || ",");
+
+    // Create a regular expression to parse the CSV values.
+    var objPattern = new RegExp(
+        (
+            // Delimiters.
+            "(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
+
+            // Quoted fields.
+            "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
+
+            // Standard fields.
+            "([^\"\\" + strDelimiter + "\\r\\n]*))"
+        ),
+        "gi"
+    );
+
+
+    // Create an array to hold our data. Give the array
+    // a default empty first row.
+    var arrData = [[]];
+
+    // Create an array to hold our individual pattern
+    // matching groups.
+    var arrMatches = null;
+
+
+    // Keep looping over the regular expression matches
+    // until we can no longer find a match.
+    while (arrMatches = objPattern.exec(strData)) {
+
+        // Get the delimiter that was found.
+        var strMatchedDelimiter = arrMatches[1];
+
+        // Check to see if the given delimiter has a length
+        // (is not the start of string) and if it matches
+        // field delimiter. If id does not, then we know
+        // that this delimiter is a row delimiter.
+        if (
+            strMatchedDelimiter.length &&
+            strMatchedDelimiter !== strDelimiter
+        ) {
+
+            // Since we have reached a new row of data,
+            // add an empty row to our data array.
+            arrData.push([]);
+
+        }
+
+        var strMatchedValue;
+
+        // Now that we have our delimiter out of the way,
+        // let's check to see which kind of value we
+        // captured (quoted or unquoted).
+        if (arrMatches[2]) {
+
+            // We found a quoted value. When we capture
+            // this value, unescape any double quotes.
+            strMatchedValue = arrMatches[2].replace(
+                new RegExp("\"\"", "g"),
+                "\""
+            );
+
+        } else {
+
+            // We found a non-quoted value.
+            strMatchedValue = arrMatches[3];
+
+        }
+
+
+        // Now that we have our value string, let's add
+        // it to the data array.
+        arrData[arrData.length - 1].push(strMatchedValue);
+    }
+
+    // Return the parsed data.
+    return (arrData);
+}
